e47f8d1d52c6384c139492dd4e4b923ccbb651e6
sim15 <sbeyzerov@gmail.com>
2022-10-14 18:29:17 -0400

    fix popover issues

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styled from 'styled-components';
 import Link from 'next/link';
-import { Popover, Button, Text } from "@nextui-org/react";
+import { Tooltip } from "@nextui-org/react";
 import catPic from '../public/cat.jpg';
 import saxophone from '../public/simonPlayingSax.png';
 
@@ -10,22 +10,16 @@ import saxophone from '../public/simonPlayingSax.png';
 const PopImg = styled.div`
     width: 12em;
     /* background-color: #f3f5f987; */
-    padding: .5em;
+    padding: .2em;
     /* max-width: 15px; */
 `;
 
 const TextPopover = ({textContent, children}) => {
-  return <Popover placement="top" disableAnimation isBordered>
-            <Popover.Trigger>
-              <a>{textContent.toString()}</a>
-            </Popover.Trigger>
-            <Popover.Content>
-              <PopImg>
-                {children}
-              </PopImg>
-              
-            </Popover.Content>
-    </Popover>
+  return <Tooltip content={<PopImg>
+      {children}
+    </PopImg>} shadow={true} css={{display: 'inline'}}>
+            <p css={{display: 'inline'}}><a>{textContent.toString()}</a></p>
+    </Tooltip>
 } 
 
 export default function Home() {
@@ -41,25 +35,29 @@ export default function Home() {
         <p>
           Hi! I&apos;m Simon, a senior at the <a target="_blank" rel="noreferrer" href="https://www.massacademy.org/">Massachusetts Academy of Math and Science at WPI</a>.
         </p>
-        <p>
+        <p >
           I&apos;m broadly interested in mathematics and computer science, with my primary research interests lying in theoretical computer science and cryptography. 
           In general, I am most excited by problems at the intersection of theory and practice.
         </p>
-        <p>
+        <p  css={{display: 'inline'}}>
           Outside of CS and Math, you may find me petting my&nbsp;
+          </p>
           <TextPopover textContent={"cat"}>
             <Image
                 src={catPic}
                 alt="Picture of my cat"
             />
-          </TextPopover>,
+          </TextPopover>
+          <p  css={{display: 'inline'}}>,
           making movies, playing minesweeper/football/
+          </p>
           <TextPopover textContent={"my saxophone"}>
             <Image
                 src={saxophone}
                 alt="Picture of me playing the saxophone"
             />
           </TextPopover>
+          <p css={{display: 'inline'}} >
           , listening to the same three songs on loop, or walking on my hands.
         </p>
         
